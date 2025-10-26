33093ba8449211e1804ab696539d607a4d5aec9d
SayantanGhosh001 <thesayantanghosh@gmail.com>
2025-10-27 01:50:05 +0530

    small changes for hosting

--- Task 6 - Todo App/Frontend/src/pages/Login.jsx
+++ Task 6 - Todo App/Frontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/authContext";
 import Navbar from "../components/Navbar";
 import "../App.css";
 
